6323b2ef33ea481ab9528a542b9075be1fcee4e6
n313893254 <n313893254@126.com>
2017-04-23 23:00:56 +0800

    import jquery

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
+import $ from 'jquery'
 
 function component() {
-  var element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'],' ');
+  var element = $('<div></div>');
+  element.html(_.join(['Hello', 'webpack'], ' '));
 
-  return element;
+  return element.get(0);
 }
 
 document.body.appendChild(component());
